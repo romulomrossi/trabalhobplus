433191d2feb5e1e11c6cb9933eb0696705cae67c
Rômulo Rossi <Rmulo Rossi>
2016-06-30 15:22:02 +0100

    flags de debug

--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@ dados.pop_back();
 
 		// cout<<endl<<"/\\antes depois\\/ "<<endl;
 		//NodeF<string> a = NodeF<string>( 3 );
-	Node<string> :: bulkLoading (dados, 5 );
+	Node<string> :: bulkLoading (dados, atol(argv[3]) );
 		 //for( vector<Dados<string> >::iterator it=dados.begin(); it!=dados.end(); ++it )
 		 	//cout<<*it<<endl;
 
--- node.hpp
+++ node.hpp
@@ -1,3 +1,5 @@
+#define DEBUG
+
 template<typename D>
 Node<D>::Node(int ordem){
 	this->pai = (Node<D>*)NULL;
@@ -18,6 +20,7 @@ Node<D> * Node<D>::split (Node<D> * esq){
 		dir->valores.push_back(esq->valores[tam]);
 	  esq->valores.erase(esq->valores.begin()+tam);
 	}
+  #ifdef DEBUG
 	cerr<<"\nesquerda: ";
 	for(unsigned int i = 0; i < esq->valores.size();i++)
 			cerr<<esq->valores[i]<<',';
@@ -25,20 +28,18 @@ Node<D> * Node<D>::split (Node<D> * esq){
 	for(unsigned int i = 0; i < dir->valores.size();i++)
 			cerr<<dir->valores[i]<<',';
 	cerr<<"\n";
+  #endif
 	return dir;
 }
 
 template<typename D>
 void Node<D>::inserePai(Node<D> *esq, Node<D> *dir){
 		if(esq->pai==(Node<D>*)NULL){
-			cerr<<"chegou aqui:";
 			esq->pai = new Node<D>(esq->ordem);
 			dir->pai = esq->pai;
 			esq->pai->filhos.push_back(esq);
 			esq->pai->filhos.push_back(dir);
-			cerr<<"né!";
 		}else{
-			cerr<<"+";
 			esq->pai->filhos.push_back(dir);
 			dir->pai = esq->pai;
 		}
@@ -46,11 +47,12 @@ void Node<D>::inserePai(Node<D> *esq, Node<D> *dir){
 			esq->pai->valores.push_back(dir->chaves[0].getChave());
 		else
 			esq->pai->valores.push_back(dir->valores[0]);
-		cerr<<".";
 		if(esq->pai->valores.size() > (unsigned int) (esq->ordem-1) ){
-			cerr<<"\ncarregado: "<<esq->pai->valores.size()<<" elementos, hora de splitar!\n";
+      #ifdef DEBUG
+  		cerr<<"\ncarregado: "<<esq->pai->valores.size()<<" elementos, hora de splitar!\n";
 			cerr<< *(esq->pai) <<"\n";
-			Node<D>::inserePai(esq->pai, split(esq->pai));
+      #endif
+  		Node<D>::inserePai(esq->pai, split(esq->pai));
 		}
 
 }
@@ -64,7 +66,6 @@ void Node<D>:: bulkLoading(vector<Dados<D> > dados, int ordem){
 	Node<D> *aux;
 
 	for(typename vector<Dados<D> >::iterator it=dados.begin()+1; it!=dados.end(); ++it ){
-		//cout<<"comparando: "<<atual->getChave()<<" e "<<it->getChave();
 		if(*atual==*it){
 			atual->addRef( it->getOffsets().back() );
 			it->clearOffsets();
@@ -73,7 +74,6 @@ void Node<D>:: bulkLoading(vector<Dados<D> > dados, int ordem){
 			novo->chaves.push_back(dados[a]);
 			atual = it ;
 			if(novo->chaves.size()>=(unsigned int)(ordem-1)/2){
-				// cout<<*atual<<endl;
 				aux = novo;
 				novo = new Node<D>(ordem);
 				novo->folha = true;
@@ -87,8 +87,10 @@ void Node<D>:: bulkLoading(vector<Dados<D> > dados, int ordem){
 	novo = first;
 	while(novo->prox != (Node<D>*)NULL ){
 		for(unsigned int i = 0; i<novo->chaves.size() && novo->folha; i++){
-			 cout<<novo->chaves[i]<<endl;
-			 Node<D>::inserePai( novo, novo->prox);
+       #ifdef DEBUG
+       cout<<novo->chaves[i]<<endl;
+       #endif
+       Node<D>::inserePai( novo, novo->prox);
 			 novo = novo->prox;
 		}
 	}
